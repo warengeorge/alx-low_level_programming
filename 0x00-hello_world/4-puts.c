9aec140936de6814ed16977e4b1806365c0acbc4
warengeorge <georgeomiebi@gmail.com>
2022-06-30 18:08:24 -0700

    Task1-6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+/* initializing string */
+  char  str1[] = "Programming is like building a multilingual puzzle\n";
+
+/* writing to stdout */
+        puts(str1);
+
+        return 0;
+}
