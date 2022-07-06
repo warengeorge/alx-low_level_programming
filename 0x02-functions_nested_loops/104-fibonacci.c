d0b26ef826e477bdc429eb4c4c9e4c694393fcf1
warengeorge <georgeomiebi@gmail.com>
2022-07-06 16:19:46 -0700

    adding files 100

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,20 +9,21 @@
 int main(void)
 {
 	int i;
-	float j[98];
+	long int j[98];
 
 	j[0] = 1;
 	j[1] = 2;
-	printf("%.0f, %.0f, ", j[0], j[1]);
+	printf("%lu, %lu, ", j[0], j[1]);
 
 	for (i = 2; i < 98; i++)
 	{
 		j[i] = j[i - 1] + j[i - 2];
 		if (i == 97)
-			printf("%.0f\n", j[i]);
+			printf("%lu", j[i]);
 		else
-			printf("%.0f, ", j[i]);
+			printf("%lu, ", j[i]);
 	}
+	printf("\n");
 
 	return (0);
 }
