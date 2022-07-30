5b3ef20192db48c180e5784861fb455da6706d34
warengeorge <georgeomiebi@gmail.com>
2022-07-30 15:04:16 -0700

    adding files

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,1 +1,40 @@
-hskka
+#include <ctype.h>
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: the number of arguments
+ * @argv: argument vector
+ * Return: nothing
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+	unsigned long int mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	else
+	{	
+	for (i = 1; i < argc; i++)
+	{
+	for (j = 0; argv[i][j] != '\0'; j++)
+	{
+	if (!isdigit(argv[i][j]))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	}
+	}	
+		mul = atol(argv[1]) * atol(argv[2]);
+		printf("%ld\n", mul);
+	}
+	return (0);
+}
